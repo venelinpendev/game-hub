e9c454e3911066e290ed9355f89489ea01a68b00
Venelin Pendev <venelin.pendev@gmail.com>
2023-05-16 07:55:13 +0100

    Introduce an API client and fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import GamesGrid from "./components/GamesGrid";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         <Show above="lg">
           <GridItem area="aside">Aside</GridItem>
         </Show>
-        <GridItem area="main">Main</GridItem>
+        <GridItem area="main">
+          <GamesGrid></GamesGrid>
+        </GridItem>
       </Grid>
     </>
   );
---
+++ src/components/GamesGrid.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GetGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const GamesGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<GetGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GamesGrid;
