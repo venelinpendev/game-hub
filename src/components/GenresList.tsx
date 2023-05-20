2ba1490a758aa1093d1a90c98de93502963f1f54
Venelin Pendev <venelin.pendev@gmail.com>
2023-05-20 12:28:11 +0100

    Display the genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,14 +1,21 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenresList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
+    <List>
       {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image src={getCroppedImageUrl(genre.image_background)} boxSize="32px" borderRadius={4} />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
