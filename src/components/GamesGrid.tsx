f36166cfa2a94a4967a3019bd62430ae53bf5937
Venelin Pendev <venelin.pendev@gmail.com>
2023-05-20 11:22:46 +0100

    Wrap the card in a generic container
    That is so we can apply the css to the container.

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import SkeletonCard from "./SkeletonCard";
+import GameCardContainer from "./GameCardContainer";
 
 const GamesGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -11,9 +12,16 @@ const GamesGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
-        {isLoading && skeletons.map(() => <SkeletonCard />)}
+        {isLoading &&
+          skeletons.map(() => (
+            <GameCardContainer>
+              <SkeletonCard />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
