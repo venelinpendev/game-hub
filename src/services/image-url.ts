d86ffdb71e5988e69215478320d763c8dc04e0f5
Venelin Pendev <venelin.pendev@gmail.com>
2023-05-23 07:16:20 +0100

    Add image placeholder for games without an image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,11 @@
-const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+import NoImagePlaceholder from "../assets/no-image-placeholder.webp";
 
-  const target = "media/"
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return NoImagePlaceholder;
+
+  const target = "media/";
   const indexToSliceOn = url.indexOf(target) + target.length;
   return url.slice(0, indexToSliceOn) + "crop/600/400/" + url.slice(indexToSliceOn);
-}
+};
 
 export default getCroppedImageUrl;
