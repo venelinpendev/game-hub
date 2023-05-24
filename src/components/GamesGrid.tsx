d83ce53d12e48d207c88e680e10b36db57ac9d5d
Venelin Pendev <venelin.pendev@gmail.com>
2023-05-24 07:21:02 +0100

    fix styles in game cards

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -16,7 +16,7 @@ const GamesGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
